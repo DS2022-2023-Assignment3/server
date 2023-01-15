dd50d4380c6635654c9d9d7d8edd407e79a04ca5
SinMax <cosmin.stretea@gmail.com>
2023-01-15 05:07:10 +0200

    feat: cors again

--- src/main.ts
+++ src/main.ts
@@ -29,7 +29,12 @@ async function bootstrap() {
   // );
   // app.use(passport.initialize());
   // app.use(passport.session());
-  app.enableCors({ optionsSuccessStatus: 200 });
+  app.enableCors({
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+  });
 
   await app.listen(8000);
 }
